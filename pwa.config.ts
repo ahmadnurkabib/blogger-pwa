7ede11dbcb2c142efa6a4c69422f1d603bb03d11
ahmadnurkabib <ahmadenterpreneur@gmail.com>
2025-05-06 19:36:59 +0700

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -17,16 +17,10 @@ export default {
   preferRelatedApplications: false,
   shortcuts: [
     {
-      name: 'Shortcut 1',
-      shortName: 'Shortcut 1',
-      description: 'Shortcut 1 description goes here...',
-      url: '/search/label/shortcut-1?utm_source=homescreen',
-    },
-    {
-      name: 'Shortcut 2',
-      shortName: 'Shortcut 2',
-      description: 'Shortcut 2 description goes here...',
-      url: '/search/label/shortcut-2?utm_source=homescreen',
+      name: 'Firmware',
+      shortName: 'Firmware',
+      description: 'Firmware premium',
+      url: '/search/label/Firmware?utm_source=homescreen',
     },
   ],
   pwa: {
